ae2320e3eae66134c0077aa8d6138d39709d9ab9
Pavel Volkov <pv781t@att.com>
2017-12-14 13:12:43 -0600

    JUnit for upper bound 100

--- src/test/java/com/amdocs/FizzBuzzTest.java
+++ src/test/java/com/amdocs/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class FizzBuzzTest extends FizzBuzz {
     @Test
-    public void testFindMultiples() throws Exception {
+    public void testFindMultiples15() throws Exception {
         int arrMult [] = {3,5};
         final int upper = 15;
         String arrLables [] = {"Fizz","Buzz"};
@@ -16,6 +16,20 @@ public class FizzBuzzTest extends FizzBuzz {
         Assert.assertEquals("Fizz: 3, 6, 9, 12", fb.getResult(0, arrLables));
         Assert.assertEquals("Buzz: 5, 10", fb.getResult(1, arrLables));
         Assert.assertEquals("FizzBuzz: 15", fb.getResultCombined( arrLables));
+    }
+
+    @Test
+    public void testFindMultiples100() throws Exception {
+        int arrMult [] = {3,5};
+        final int upper = 100;
+        String arrLables [] = {"Fizz","Buzz"};
+
+        FizzBuzz fb = new FizzBuzz(upper);
+        fb.findMultiples(arrMult, upper);
+
+        Assert.assertEquals("Fizz: 3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99", fb.getResult(0, arrLables));
+        Assert.assertEquals("Buzz: 5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100", fb.getResult(1, arrLables));
+        Assert.assertEquals("FizzBuzz: 15, 30, 45, 60, 75, 90", fb.getResultCombined( arrLables));
     }
 
 }
