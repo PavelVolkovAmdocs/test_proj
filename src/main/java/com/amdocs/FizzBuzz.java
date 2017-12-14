6e8c5e147c09755bebad5b86049e8219b241e014
Pavel Volkov <pv781t@att.com>
2017-12-14 12:01:13 -0600

    update mvn structure

---
+++ src/main/java/com/amdocs/FizzBuzz.java
@@ -0,0 +1,69 @@
+package com.amdocs;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FizzBuzz {
+    // Holds the multiples. The code can be scaled up by adding more multiples to the list...
+    private int arrMult [] = {3,5};
+    // Tags for multiples to print. The size must be same as arrMult.
+    // We do not check the size in this version as both arrays are hardcoded...
+    private String arrLables [] = {"Fizz","Buzz"};
+
+    // we will use LinedList as an optimal collection
+    // for iterations and appending to the tail
+    private List resPerMultiple [] = null;
+    // this is to hold results for combinations of multiples...
+    private List resCombined = new LinkedList();
+
+    // public constructor...
+    public FizzBuzz(int upperBound){
+        System.out.println("FizzBuzz Results");
+        System.out.println("==============================");
+
+        //initialize List array as LinkedLists
+        resPerMultiple = new List[upperBound];
+        for (int i = 0; i < upperBound; i++){
+            resPerMultiple [i] = new LinkedList<Integer>();
+        }
+        findMultiples(arrMult, upperBound);
+    }
+
+    //generic function for finding multiples based on given multiples array.
+    //passing multiples as argument to generalize the function...
+    private void findMultiples( int i_arrMult [], int upperBound ){
+        for (int i = 1; i < upperBound; i++ ){
+            for(int j = 0; j < i_arrMult.length; j++){
+                boolean allMult = true;
+                if (i % i_arrMult[j] == 0){
+                    resPerMultiple[j].add(i);
+                }
+                else{
+                    allMult = false;
+                }
+                // add to combinations and remove from each list ...
+                if(allMult){
+
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String sUserInput = "";
+        boolean booProceed = true;
+        do  {
+            System.out.println("Enter FizzBuzz upper boundary:");
+            sUserInput = in.next();
+            try{
+                Integer inpBound = Integer.parseInt(sUserInput);
+                new FizzBuzz(inpBound);
+                booProceed = false;
+            }catch(NumberFormatException nfe){
+                System.out.println("ERROR: FizzBuzz upper boundary must be an integer !!!");
+            }
+        } while(booProceed);
+        //System.out.println("Completed...");
+    }
+}
