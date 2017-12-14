094fb7b46ab640013129d120c10bc5100a2e2894
Pavel Volkov <pv781t@att.com>
2017-12-14 11:47:24 -0600

    iteration 1.1

--- src/com/amdocs/FizzBuzz.java
+++ src/com/amdocs/FizzBuzz.java
@@ -1,8 +1,30 @@
 package com.amdocs;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
+    // public constructor...
+    public FizzBuzz(int upperBound) {
+        System.out.println("FizzBuzz Results");
+        System.out.println("==============================");
+        System.out.println(upperBound);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String sUserInput = "";
+        boolean booProceed = true;
+        do  {
+            System.out.println("Enter FizzBuzz upper boundary:");
+            sUserInput = in.next();
+            try{
+                Integer inpBound = Integer.parseInt(sUserInput);
+                new FizzBuzz(inpBound);
+                booProceed = false;
+            }catch(NumberFormatException nfe){
+                System.out.println("ERROR: FizzBuzz upper boundary must be an integer !!!");
+            }
+        } while(booProceed);
+        //System.out.println("Completed...");
     }
 }
